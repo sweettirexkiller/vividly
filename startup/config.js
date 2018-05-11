7ae972310ec226ed38fd3e3096fc1ef475010397
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-11 15:58:31 +0200

    intergration api tests

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,7 @@
-const config = require('vidly/startup/config');
+const config = require('config');
 
 module.exports = function() {
   if (!config.get('jwtPrivateKey')) {
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
   }
-}
+};
---
+++ tests/intefration/genres.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const {Genre} = require('../../models/genre');
+let server;
+
+describe('/api/genres', () => {
+    beforeEach(async () => {
+        server = require('../../index');
+        await Genre.collection.insertMany([
+            {name: 'genre1'},
+            {name: 'genre2'},
+            {name: 'genre3'}
+        ]);
+    });
+    afterEach(async () => {
+        await Genre.remove({});
+        await server.close();
+    });
+
+    describe('GET /', () => {
+        it('should return all genres', async () => {
+            let res = await request(server).get('/api/genres');
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(3);
+        });
+    });
+
+    describe('GET /:id', () => {
+        it('should return genre if valid id passed', async () => {
+            const genre = new Genre({name: 'genre1'});
+            await genre.save();
+
+            const res = await request(server).get('/api/genres/' + genre._id);
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
+
+
+        });
+    });
+});
